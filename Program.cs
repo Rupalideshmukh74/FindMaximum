5102c95ebe79cd0b6fc6831d8bb51f4b6c57944a
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-18 21:40:34 +0530

    [Rupali]Feat:find integer of 3 integer no

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace FindMaximum
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Find Max Program");
+            //Calling Class to call a Function
+            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+            int outputInteger = maximumNumberCheck.MaximumIngerNumber(11, 22, 33);
+
+            //Display Output
+            Console.WriteLine("Maximum Number of Integer : " + outputInteger);
         }
     }
 }
